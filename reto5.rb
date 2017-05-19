6c050c5df98c31f1cf510d764ff9340bd775979d
cristian-castrillon <cristian.castrillon@gmail.com>
2017-05-19 13:56:04 -0500

    [ADD] Archivo reto5.rb el cual contienen los comandos de ruby para iniciar el juego

---
+++ reto5.rb
@@ -0,0 +1,14 @@
+require "./juego"
+
+puts "Bienvenido a reto 5, Para jugar, solo ingresa el termino correcto para cada una de las definiciones, Listo? Vamos!"
+
+
+
+nuevo_juego = Juego.new
+nuevo_juego.empieza_juego
+
+puts "Bien hecho!"
+puts "Hasta la vista."
+puts "•_•)"
+puts "( •_•)>⌐■ -■"
+puts "(⌐■ _■ )"
